b4144d4065efa2cc3174ec2b2dd48c27ceae6e59
Mutz <mutz@mutzs.org>
2025-08-16 00:41:30 +0200

    Cap frame rate to 60 FPS using Pygame clock.

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ from constants import *
 
 def main():
     pygame.init()
+    dt = 0.0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color="black")
+
+        screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60.0) / 1000.0
+
 
 
 if __name__ == "__main__":
