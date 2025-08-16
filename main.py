dea03c2ca6660124b4012bdfe3d8564f51e419c8
Mutz <mutz@mutzs.org>
2025-08-16 16:07:11 +0200

    Remove unused `asteroidfield` variable assignment in `main.py`.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 import pygame
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,12 +16,16 @@ def main():
     # create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # assign Player Class
     Player.containers = updatable, drawable
+    Asteroid.containers = updatable, drawable, asteroids
+    AsteroidField.containers = updatable
 
     # instantiate Player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     # Game loop
     while True:
