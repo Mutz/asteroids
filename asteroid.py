b124a85bf6c21f3718ade62740bfc6371ffbe889
Mutz <mutz@mutzs.org>
2025-08-16 17:53:51 +0200

    Implement asteroid splitting: replace `kill` with `split` logic and create smaller asteroids upon collision.

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
-from circleshape import CircleShape
 import pygame
+import random
+from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,4 +12,27 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
-
+    
+    def split(self):
+        self.kill()
+        
+        # If the asteroid is too small, don't split
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # Generate random angle between 20 and 50 degrees
+        random_angle = random.uniform(20, 50)
+        
+        # Create two new velocity vectors rotated in opposite directions
+        velocity1 = self.velocity.rotate(random_angle)
+        velocity2 = self.velocity.rotate(-random_angle)
+        
+        # Calculate new radius for smaller asteroids
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        # Create two new asteroids at the current position
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = velocity1 * 1.2  # Make it move faster
+        
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = velocity2 * 1.2  # Make it move faster
